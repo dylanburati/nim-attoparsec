54300dc9188396fcff95e08912b891645f51aa8e
Dylan Burati <dylanburati@protonmail.com>
2023-03-08 07:51:27 -0500

    Rename methods to avoid ambiguity with std/streams

--- src/nim_parsec.nim
+++ src/nim_parsec.nim
@@ -10,44 +10,44 @@ import std/strutils
 import std/sequtils
 import std/sugar
 
-type 
-  ResultKind = enum
-    ekLeft, ekRight
-  Result[T] = ref object
-    case kind*: ResultKind 
-    of ekLeft: msg*: string
-    of ekRight: val*: T
-  
+type
+  ResultKind* = enum
+    rkLeft, rkRight
+  Result*[T] = ref object
+    case kind*: ResultKind
+    of rkLeft: msg*: string
+    of rkRight: val*: T
+
 proc Ok[T](val: T): Result[T] =
-  return Result[T](kind: ekRight, val: val)
+  return Result[T](kind: rkRight, val: val)
 
 proc Fail[T](msg: string): Result[T] =
-  return Result[T](kind: ekLeft, msg: msg)
+  return Result[T](kind: rkLeft, msg: msg)
 
 proc map*[T, R](this: Result[T], f: T -> R): Result[R] =
   ## Maps a Result[T] to Result[R] by applying a function to a contained
   ## Ok value, leaving an Err value untouched.
   case this.kind
-  of ekLeft:
+  of rkLeft:
     return Fail[R](this.msg)
-  of ekRight: 
+  of rkRight:
     return Ok(f(this.val))
 
 proc tryMap*[T, R](this: Result[T], f: T -> Result[R]): Result[R] =
   ## Calls op if the result is Ok, otherwise returns the Err value of this.
   case this.kind
-  of ekLeft:
+  of rkLeft:
     return Fail[R](this.msg)
-  of ekRight: 
+  of rkRight:
     return f(this.val)
 
 proc validate*[T](this: Result[T], msg: string, f: T -> bool): Result[T] =
   ## Returns Err(msg) if the result is Ok but the value inside of it does not match
   ## the predicate.
   case this.kind
-  of ekLeft:
+  of rkLeft:
     return Fail[T](this.msg)
-  of ekRight: 
+  of rkRight:
     if not f(this.val):
       return Fail[T](msg)
     else:
@@ -55,15 +55,15 @@ proc validate*[T](this: Result[T], msg: string, f: T -> bool): Result[T] =
 
 proc `$`*[T](this: Result[T]): string =
   case this.kind
-  of ekLeft: return fmt"<Left {this.msg}>"
-  of ekRight: return fmt"<Right {this.val}>"
+  of rkLeft: return fmt"<Left {this.msg}>"
+  of rkRight: return fmt"<Right {this.val}>"
 
 proc `==`*[T](this: Result[T], other: Result[T]): bool =
   case this.kind
-  of ekLeft:
-    return other.kind == ekLeft
-  of ekRight:
-    return other.kind == ekRight and other.val == this.val
+  of rkLeft:
+    return other.kind == rkLeft
+  of rkRight:
+    return other.kind == rkRight and other.val == this.val
 
 type
   State = tuple
@@ -82,11 +82,27 @@ proc newParser[T](f: State -> (State, Result[T])): Parser[T] =
 proc `$`*(this: Parser): string =
   return fmt("<Parser:>")
 
-proc runParser[T](this: Parser[T], s: string): (State, Result[T]) =
+proc runParser*[T](this: Parser[T], s: string, debug: bool = false): (State, Result[T]) =
   var reference: ref[string]
   new(reference)
   reference[] = s
   let state0: State = (reference, 0)
+  if debug:
+    let (last, res) = this.f(state0)
+    if res.kind == rkRight:
+      return (last, res)
+    else:
+      var i = 0
+      var line = 1
+      var col = -1
+      while i >= 0 and i < s.len:
+        let i2 = s.find('\n', i)
+        if last.pos <= i2:
+          col = last.pos - i
+          break
+        i = i2 + 1
+        line += 1
+      return (last, Fail[T](fmt"{line}:{col} {res.msg}"))
   return this.f(state0)
 
 proc parse*[T](this: Parser[T], s: string): Result[T] =
@@ -124,38 +140,35 @@ proc andThen*[T1, T2](p1: Parser[T1], p2gen: T1 -> Parser[T2]): Parser[T2] =
   proc inner(s: State): (State, Result[T2]) =
     let (nxt1, res1) = p1.f(s)
     case res1.kind
-    of ekLeft:
+    of rkLeft:
       return (nxt1, Fail[T2](res1.msg))
-    of ekRight:
+    of rkRight:
       let p2 = p2gen(res1.val)
       let (nxt2, res2) = p2.f(nxt1)
       case res2.kind
-      of ekLeft:
+      of rkLeft:
         return (nxt2, Fail[T2](fmt"andThen[{s.pos},{nxt1.pos}] {res2.msg}"))
-      of ekRight:
+      of rkRight:
         return (nxt2, res2)
   return newParser(inner)
 
 proc andAdd*[T](p1: Parser[T], p2: Parser[T]): Parser[T] =
   ## Succeeds with the value `val1 & val2` if the first parser succeeds with
-  ## `val1` and the second parser succeeds with `val2`. Like Haskell Semigroup
-  ## `<>`.
+  ## `val1` and the second parser succeeds with `val2`.
   proc gen(parsed1: T): Parser[T] =
     return p2.map((parsed2) => parsed1 & parsed2)
   return p1.andThen(gen)
 
 proc andAdd*(p1: Parser[string], p2: Parser[char]): Parser[string] =
   ## Succeeds with the value `val1 & val2` if the first parser succeeds with
-  ## `val1` and the second parser succeeds with `val2`. Like Haskell Semigroup
-  ## `<>`.
+  ## `val1` and the second parser succeeds with `val2`.
   proc gen(parsed1: string): Parser[string] =
     return p2.map((parsed2) => parsed1 & parsed2)
   return p1.andThen(gen)
 
 proc andAdd*(p1: Parser[char], p2: Parser[string]): Parser[string] =
   ## Succeeds with the value `val1 & val2` if the first parser succeeds with
-  ## `val1` and the second parser succeeds with `val2`.  Like Haskell Semigroup
-  ## `<>`.
+  ## `val1` and the second parser succeeds with `val2`.
   proc gen(parsed1: char): Parser[string] =
     return p2.map((parsed2) => parsed1 & parsed2)
   return p1.andThen(gen)
@@ -166,14 +179,14 @@ proc orElse*[T](p1, p2: Parser[T]): Parser[T] =
   proc inner(s: State): (State, Result[T]) =
     let (nxt, res) = p1.f(s)
     case res.kind
-    of ekRight:
+    of rkRight:
       return (nxt, res)
-    of ekLeft:
+    of rkLeft:
       let (nxt, res) = p2.f(s)
       case res.kind
-      of ekLeft:
+      of rkLeft:
         return (nxt, Fail[T](fmt"orElse {res.msg}"))
-      of ekRight:
+      of rkRight:
         return (nxt, res)
   return newParser(inner)
 
@@ -224,7 +237,7 @@ proc failp*[T](msg: T): Parser[T] =
 
 proc endOfInputImpl(s: State): (State, Result[void]) =
   if s.pos >= s.str[].len:
-    return (s, Result[void](kind: ekRight))
+    return (s, Result[void](kind: rkRight))
   else:
     return (s, Fail[void]("endOfInput"))
 
@@ -245,9 +258,9 @@ proc many*[T](parser: Parser[T]): Parser[seq[T]] =
       let (nxt, res) = parser.f(mys)
       mys = nxt
       case res.kind
-      of ekLeft:
+      of rkLeft:
         return (mys, Ok(fullparsed))
-      of ekRight:
+      of rkRight:
         fullparsed.add(res.val)
   return newParser(inner)
 
@@ -261,9 +274,9 @@ proc count*[T](parser: Parser[T], n: int): Parser[seq[T]] =
       let (nxt, res) = parser.f(mys)
       mys = nxt
       case res.kind
-      of ekLeft:
+      of rkLeft:
         return (mys, Fail[seq[T]](fmt"count[{s.pos}] {res.msg}"))
-      of ekRight:
+      of rkRight:
         fullparsed.add(res.val)
     return (mys, Ok(fullparsed))
   return newParser(inner)
@@ -271,7 +284,7 @@ proc count*[T](parser: Parser[T], n: int): Parser[seq[T]] =
 proc many1*[T](parser: Parser[T]): Parser[seq[T]] =
   ## Returns a parser which applies the base parser one or more times.
   return count(parser, 1) &> many(parser)
-    
+
 proc `*`*[T](this: Parser[T], n: int): Parser[seq[T]] =
   ## Operator form of `count`.
   return count(this, n)
@@ -285,14 +298,14 @@ proc manyTill*[T, Any](parser: Parser[T], endParser: Parser[Any]): Parser[seq[T]
     var fullparsed: seq[T]
     while true:
       let (post, endRes) = endParser.f(mys)
-      if endRes.kind == ekRight:
+      if endRes.kind == rkRight:
         return (post, Ok(fullparsed))
       let (nxt, res) = parser.f(mys)
       mys = nxt
       case res.kind
-      of ekLeft:
+      of rkLeft:
         return (nxt, Fail[seq[T]](fmt"manyTill[{s.pos}] {res.msg}"))
-      of ekRight:
+      of rkRight:
         fullparsed.add(res.val)
   return newParser(inner)
 
@@ -338,7 +351,7 @@ proc peekImpl(s: State): (State, Result[Option[char]]) =
   else:
     return (s, Ok(some(s.str[s.pos])))
 
-let peek*: Parser[Option[char]] = newParser(peekImpl)
+let peekCharP*: Parser[Option[char]] = newParser(peekImpl)
 ## Succeeds with `some(c)` if a character `c` is left in the input. Succeeds
 ## with none[char]() if there is no more input.
 
@@ -390,7 +403,7 @@ proc takeWhile1*(test: char -> bool): Parser[string] =
 
 # proc sepBy*(sep: Parser, parser: Parser): Parser =
 #   let myparsed = @[""]
-#   let noneparser = newParser(f=(_) => Result(kind: ekRight, val: (parsed: myparsed, remaining: "")))
+#   let noneparser = newParser(f=(_) => Result(kind: rkRight, val: (parsed: myparsed, remaining: "")))
 #   return sepBy1(sep, parser) | noneparser
 
 # proc sepBy*(sep: Parser, comb: proc(): Parser): Parser =
@@ -508,12 +521,12 @@ when isMainModule:
   let jsonSpace = {' ', '\t', '\r', '\n'}
   let skipSpace = takeWhile((c) => c in jsonSpace)
   let jsonStops = jsonSpace + {',', ']', '}'}
+  # eof counts as a JSON stop too
+  let expectJsonStop = peekCharP.validate("expected JSON stop", (mbc) => mbc.get(' ') in jsonStops)
   let parseIntRep = optional(stringp("-"), "") &> orElse(
     stringp("0"),
     charp({'1'..'9'}) &> takeWhile((c) => c in Digits)
   )
-  # eof counts as a JSON stop too
-  let expectJsonStop = peek.validate("expected JSON stop", (mbc) => mbc.get(' ') in jsonStops)
   let parseInt = (parseIntRep <* expectJsonStop).map(parseBiggestInt)
   let parseFracRep1 = charp('.') &> takeWhile1((c) => c in Digits)
   let parseFracRep2 = charp({'e', 'E'}) &> optional(stringp("-") | stringp("+"), "") &> takeWhile1((c) => c in Digits)
@@ -536,7 +549,7 @@ when isMainModule:
     of 'a'..'f': return some((st[0] - 1, st[1]*16 + 10 + c.ord - 'a'.ord))
     of 'A'..'F': return some((st[0] - 1, st[1]*16 + 10 + c.ord - 'A'.ord))
     else: return none[(int, int)]()
-    
+
   let parseEscape = charp('\\') >> anyChar.andThen(proc (c: char): Parser[string] =
     case c
     of '"', '\\', '/': return constp($c)
@@ -561,7 +574,7 @@ when isMainModule:
       )
     )
   let parseString = parseStringRecur().map(l => l.join(""))
-  let parseToken = skipSpace >> peek.andThen(proc (mbc: Option[char]): Parser[Token] =
+  let parseToken = skipSpace >> peekCharP.andThen(proc (mbc: Option[char]): Parser[Token] =
     if mbc == some('{'):
       return charp('{') >> constp(newJsonObjectStart())
     elif mbc == some('}'):
@@ -583,7 +596,6 @@ when isMainModule:
     elif mbc == some('"'):
       return charp('"') >> parseString.map(newJsonString)
     else:
-      # order is important; could be solved by forcing both to peek and expect one of ",]} "
       return parseFloat.map(newJsonFloat) | parseInt.map(newJsonInt)
   )
   let tokenizer = manyTill(parseToken <* skipSpace, endOfInput)
@@ -592,16 +604,16 @@ when isMainModule:
       echo fmt"input: {inp}"
       let (st, res) = runParser(tokenizer, inp)
       case res.kind
-      of ekLeft:
+      of rkLeft:
         echo fmt"""Left "{res.msg}" @ {st.pos}"""
-      of ekRight:
+      of rkRight:
         echo "Right " & mapIt(res.val, $it).join("")
     except Exception as e:
       echo getStackTrace(e)
-  
+
   import std/os
   import std/streams
-  for path in walkFiles("/home/dylan/Downloads/4cs/JSONTestSuite/test_parsing/*.json"):
+  for path in walkFiles("../JSONTestSuite/test_parsing/*.json"):
     if path.splitPath().tail.startsWith("y_"):
       let strm = openFileStream(path, fmRead)
       let text = strm.readAll()
@@ -609,122 +621,3 @@ when isMainModule:
       echo ""
       echo path
       showParse(text)
-
-let ignored = """
-
-proc smashTransformer(l: seq[string]): seq[string] =
-  @[join(l, "")]
-
-let letter = anyOf(strutils.Letters)
-let lletter = anyOf({'a'..'z'})
-let uletter = anyOf({'A'..'Z'})
-let digit = anyOf(strutils.Digits)
-let hexdigit, hexstr = anyOf(strutils.HexDigits)
-let ws = anyOf(strutils.Whitespace)
-let letters = many1(letter)
-let digits = many1(digit)
-let word = letters.map(smashtransformer)
-let alphanumword = many1(letter >> (letters|digits)).map(smashtransformer)
-let between = proc(p1, p2, p3: Parser): Parser =
-  return p1 >> p2 >> p3
-let surroundedBy = proc(surparser, contentparser: Parser): Parser =
-  return surparser >> contentparser >> surparser
-
-
-when isMainModule:
-  let aParser = charp('a')
-  let bParser = charp('b')
-  echo $aParser.parse("abc")
-  echo $bParser.parse("bca")
-
-  let abParser = charp('a') >> charp('b')
-  echo $abParser.parse("abc")
-
-  let aorbParser = charp('a') | charp('b')
-  echo $aorbParser.parse("acd")
-  echo $aorbParser.parse("bcd")
-
-  let abcParser = parseString("abc")
-  echo $abcParser.parse("abcdef")
-
-  let manyA = many(charp('a'))
-  echo $manyA.parse("aaab")
-  echo $manyA.parse("bbb")
-
-  let manyA1 = many1(charp('a'))
-  echo $manyA1.parse("aaab")
-  echo $manyA1.parse("bbb")
-
-  let manyDigits = many1(digit)
-  echo $manyDigits.parse("1234")
-
-  let commaseparatednums = sep_by(charp(',').suppress(), digit)
-  echo $commaseparatednums.parse("1,2,4")
-
-  #let greetparser = letters >> charp(',') >> many(ws) >> letters
-  let greetparser = word >> charp(',').suppress() >> many(ws).suppress() >> word >> optionally(charp('!'))
-  echo $greetparser.parse("Hello,   World")
-  echo $greetparser.parse("Hello,   World!")
-
-
-  echo $(letter*3).parse("abc")
-
-  let uuidsample = "db9674c4-72a9-4ab9-9ddd-1d641a37cde4"
-  let uuidparser =(hexstr*8).map(smashtransformer) >> charp('-') >> (hexstr*4).map(smashtransformer) >> charp('-') >>  (hexstr*4).map(smashtransformer) >> charp('-') >> (hexstr*4).map(smashtransformer) >> charp('-') >> (hexstr*12).map(smashtransformer)
-  echo $uuidparser.parse(uuidsample)
-  
-
-  let sur3pipe = surroundedBy(charp('|'), charp('3'))
-  echo $sur3pipe.parse("|3|")
-
-  let paren3 = between(charp('('), charp('3'), charp(')') )
-  echo paren3.parse("(3)")
-
-  # recursive lang ints and list of ints or lists
-  type 
-    LangElemKind = enum
-      leChr, leList
-    LangElem = ref object
-      case kind*: LangElemKind 
-      of leChr: c*: char
-      of leList: l*: seq[LangElem]
-  
-  proc `$`*(this: LangElem): string =
-    case this.kind
-    of leChr: return fmt"<Char {this.c}>"
-    of leList: return fmt("<List: {this.l}>")
-
-  proc `==`*(this: LangElem, other: LangElem): bool =
-    return this.kind == other.kind
-
-  proc parseToNimData(data: seq[string]) : LangElem =
-    result = LangElem(kind: leList, l: @[])
-    let dataIsList = data[0][0] == '['
-    for el in data:
-      var firstchr = el[0]
-      if firstchr.isAlphaAscii():
-        var elem = LangElem(kind: leChr, c: firstchr)
-        if dataIsList == false:
-          return elem
-        else:
-          result.l[result.l.len-1].l.add(LangElem(kind: leChr, c: firstchr))
-
-      elif firstchr == '[':
-        result.l.add(LangElem(kind: leList, l: @[]))
-      
-
-  var listp: Parser
-  var valref = (proc(): Parser =letters|listp)
-
-  listp = charp('[') >> sep_by(charp(',').suppress(), many(valref)) >> charp(']')
-  var valp = valref()
-
-  var inps = @["a", "[a,b]", "[a,[b,c]]"]
-  for inp in inps:
-    echo &"inp : {inp}"
-    let parsed = valp.parse(inp)
-    if parsed.kind == ekRight:
-      let data = parsed.val.parsed
-      echo "parsed data: " & data
-      echo inp, " => ", $parseToNimData(data)
-"""
